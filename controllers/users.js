a5e030ca704848c4fa0fcd39f5ff273ff066cb4e
Cynthia Chaves <cynthia_chaves@aol.com>
2024-10-28 12:29:13 -0400

    Completed lab

---
+++ controllers/users.js
@@ -0,0 +1,13 @@
+// const express = require('express');
+// const router = express.Router();
+
+// const User = require('../models/user.js');
+
+// router.get('/', (req, res) => {
+//   const allUsers = User.findById();
+//   res.render('users/index.ejs',{
+//     user: allUsers,
+//   })
+// });
+
+// module.exports = router;
--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
 
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+
 const authController = require('./controllers/auth.js');
+const foodsController = require('./controllers/foods.js');
+//const usersController = require('./controllers/users.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
@@ -19,7 +24,6 @@ mongoose.connection.on('connected', () => {
 
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
-// app.use(morgan('dev'));
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -28,21 +32,19 @@ app.use(
   })
 );
 
+app.use(passUserToView);
+
 app.get('/', (req, res) => {
   res.render('index.ejs', {
     user: req.session.user,
   });
 });
 
-app.get('/vip-lounge', (req, res) => {
-  if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
-  } else {
-    res.send('Sorry, no guests allowed.');
-  }
-});
+//app.use('/users', usersController);
 
 app.use('/auth', authController);
+app.use(isSignedIn);
+app.use('/users/:userId/foods', foodsController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
